7d0b236fd047f476b19a9c4ae5ca8ad426820606
Marcos <p000marcolima@prservicos.com.br>
2022-04-10 14:34:09 -0300

    Use spark 3

--- get-sw.sh
+++ get-sw.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-HADOOP_VERSION='3.3.0'
-SPARK_VERSION='3.0.3'
+HADOOP_VERSION='3.3.1'
+SPARK_VERSION='3.2.1'
 SPARK_HADOOP_COMPAT='3.2'
+SCALA_VERSION='-scala2.13'
 
 #APACHE_MIRROR="http://mirror.dsrg.utoronto.ca/apache/"
 APACHE_MIRROR="http://ftp.unicamp.br/pub/apache/"
@@ -10,7 +11,7 @@ APACHE_MIRROR="http://ftp.unicamp.br/pub/apache/"
 
 HADOOP_TARFILE="hadoop-${HADOOP_VERSION}.tar.gz"
 HADOOP_APACHE_PATH="hadoop/common/hadoop-${HADOOP_VERSION}/${HADOOP_TARFILE}"
-SPARK_TARFILE="spark-${SPARK_VERSION}-bin-hadoop${SPARK_HADOOP_COMPAT}.tgz"
+SPARK_TARFILE="spark-${SPARK_VERSION}-bin-hadoop${SPARK_HADOOP_COMPAT}${SCALA_VERSION}.tgz"
 SPARK_APACHE_PATH="spark/spark-${SPARK_VERSION}/${SPARK_TARFILE}"
 
 mkdir -p sw
